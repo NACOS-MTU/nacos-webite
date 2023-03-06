b5ee6c4539739b43b58fba60d52275deb5a244f3
John Olatubosun <toluolatubosun@gmail.com>
2023-03-06 13:21:12 +0100

    chore: added prettier

--- pages/HAbout.tsx
+++ pages/HAbout.tsx
@@ -1,85 +1,41 @@
 import { AiOutlineMail } from "react-icons/ai";
 
-
-
-
-
 export default function HAbout() {
     return (
         <>
-            <div
-                className="p-10 relative"
-            >
-                <div
-                    className="text-green-400 font-semibold"
-                >
-                    ABOUT US
-                </div>
+            <div className="p-10 relative">
+                <div className="text-green-400 font-semibold">ABOUT US</div>
 
-                <div
-                    className="text-black font-extrabold text-3xl "
-                >
+                <div className="text-black font-extrabold text-3xl ">
                     NACOS (MTU CHAPTER)
-                    <p
-                        className="h-1 w-12 rounded-md  bg-green-500"
-                    />
+                    <p className="h-1 w-12 rounded-md  bg-green-500" />
                 </div>
 
-                <div
-                    className="pt-5"
-                >
-                    <div
-                        className="grid grid-cols-2"
-                    >
-                        <div
-                        className=" lg:col-span-1 col-span-2"
-                        >
-                            Nigerian Association of computing Students (Nacos) is the umbrella body for students studying
-                            computer science , computing engineering, information systems, cyber security, software engineering, telecommunications engineerign and all IT-related courses in Nigeria and have been
-                            in existence for over two decades and Nacos can boast of its presence in almost all tertiary institution in Nigeria(including privately owned ones).
-
-                            <p
-                                className="pt-3"
-                            >
-                                With a memrbership strength of about Three Hundred and Fifty Thousand(350,00) members in over
-                                two hundred and fifty(250) local chapters in different tertiary instutions (Universities, Polytechnics and College of Education)
-                                across the six(6) geo-political zones of the federation,
-                                thus making the Association the largest and most organized students board in Africa
+                <div className="pt-5">
+                    <div className="grid grid-cols-2">
+                        <div className=" lg:col-span-1 col-span-2">
+                            Nigerian Association of computing Students (Nacos) is the umbrella body for students studying computer science , computing engineering, information systems, cyber security,
+                            software engineering, telecommunications engineerign and all IT-related courses in Nigeria and have been in existence for over two decades and Nacos can boast of its
+                            presence in almost all tertiary institution in Nigeria(including privately owned ones).
+                            <p className="pt-3">
+                                With a memrbership strength of about Three Hundred and Fifty Thousand(350,00) members in over two hundred and fifty(250) local chapters in different tertiary instutions
+                                (Universities, Polytechnics and College of Education) across the six(6) geo-political zones of the federation, thus making the Association the largest and most
+                                organized students board in Africa
                             </p>
                         </div>
 
-
-
-                        <img
-                            src="/w-logo.jpg"
-                            className="hidden lg:block absolute right-10 bottom-20 z-10 h-[400px]"
-                        />
+                        <img src="/w-logo.jpg" className="hidden lg:block absolute right-10 bottom-20 z-10 h-[400px]" />
                     </div>
-
-
-
-
-
-
                 </div>
 
-
-
-                <div
-                    className=" pb-5 pt-5 "
-                >
-
-
-                    Nacospresident@mtu.edu.ng<span>  <AiOutlineMail /></span>
-
+                <div className=" pb-5 pt-5 ">
+                    Nacospresident@mtu.edu.ng
+                    <span>
+                        {" "}
+                        <AiOutlineMail />
+                    </span>
                 </div>
-
-
-
-
-
             </div>
-
         </>
-    )
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,27 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import DefaultLayout from '../components/DefaultLayout'
-import styles from '../styles/Home.module.css'
-import HAbout from './HAbout'
-import HEvents from './HEvents'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import DefaultLayout from "../components/DefaultLayout";
+import styles from "../styles/Home.module.css";
+import HAbout from "./HAbout";
+import HEvents from "./HEvents";
 
 const Home: NextPage = () => {
-  return (
-   <DefaultLayout>
-    <>
-    
-    <div  className='w-full bg-red-500 mt-5 text-black'>
+    return (
+        <DefaultLayout>
+            <>
+                <div className="w-full bg-red-500 mt-5 text-black">THIS IS AN ELMO SONG</div>
 
-      THIS IS AN ELMO SONG 
-    </div>
+                <div>
+                    <HEvents />
+                </div>
 
+                <div>
+                    <HAbout />
+                </div>
+            </>
+        </DefaultLayout>
+    );
+};
 
-
-    <div>
-      <HEvents/>
-    </div>
-    
-
-    <div>
-      <HAbout/>
-    </div>
-    </>
-   </DefaultLayout>
-  )
-}
-
-export default Home
+export default Home;
